b00e4b443a1663af7ab707eb0cdda28a7801f750
JakubKarp <jakub.karp@innespacery.pl>
2017-06-26 10:41:58 +0200

    pomoc

--- js/main.js
+++ js/main.js
@@ -1,64 +1,99 @@
-//deklaracja zmiennych
-/*var carousel = $("#carousel");
+//1.  deklaracja zmiennych
+    /*var carousel = $("#carousel");
     var carouselList = $(".ul-class");
     var carouselDots = $(".dots");*/
   
 
 var carousel = document.getElementById("carousel");
-//var carouselList = document.getElementsByClassName('ulcarousel');
-var carouselNav = document.getElementsByClassName('navdot');
+var carouselLi = document.querySelectorAll("ul.ulcarousel > li");
+var carNav = document.querySelector("ul.navdot");
 
-//ile obrazów tyle kropek
-/*carouselList.find("li").each(function(){
-       //console.log("test");
+
+
+//2. ile obrazów tyle kropek
+    /*carouselList.find("li").each(function(){
         carouselDots.append("<li></li>");
     });*/
-var test = document.querySelectorAll("ul.ulcarousel > li");
-    console.log(test.length);
-//carouselList.forEach(){
-  //  carouselNav.innerHTML("<li></li>");
-//};
+
+for (i = 0; i < carouselLi.length; i++) {
+    //console.log(i);
+    var liElement = document.createElement("li");
+    carNav.appendChild(liElement); 
+    //console.log(liElement);
+
+};
 
 
-/*
-
-//oryginał
-$(function() {
-      
-    
-    
-    //pierwsza kropka aktywna
-
-    var dot = $(".dots li");
+//3. pierwsza kropka aktywna
+    /*var dot = $(".dots li");
     dot.first().addClass("active");
-    
-    //po kliknięciu zmiana statusu kropki
-    dot.click(function() {
+    */
+    var dotAll = carNav.children;
+        //console.log(dotAll);
+    //var dotFirst = carNav.children[0];
+        //console.log(dotFirst);    
+    //dotFirst.classList.add('active');
+    carNav.children[0].classList.add('active');
+
+//4. po kliknięciu zmiana statusu kropki
+    /*dot.click(function() {
         var target = $(this).index();
         //console.log(target);
         slideChange(target);
-    });
+    });*/
+
+
+// PIOTR. NIE MAM POJECIA, JAK MANIPULOWAĆ KLASĄ ACTIVE PO KLIKNIĘCIU - DODAWAĆ JĄ LUB ODEJMOWAĆ. pLEASE PODPOWIEDŹ    
+    var dotArray = [].slice.call(dotAll);
+    for (j = 0; j < dotArray.length; j++){
+        var dotArrayIteration = dotArray[j]; 
+            console.log(dotArrayIteration);  
+        dotArrayIteration.addEventListener("click", zmianaSlajdu);
+            function zmianaSlajdu(){ 
+                
+               
+                //dotArrayIteration.classList.remove('active');
+                dotArrayIteration.querySelectorAll(dotAll).classList.add('active'); // to nie działa w ogóle
+                dotArrayIteration.classList.add('active'); //to działa na ostatnią kropkę
+            
+            };
+
+    };
+
+
+//console.log(dotArray);
     
-    //slajdowanie zdjęć po kliknięciu kolejnych kropek
-    function slideChange(target) {
+          
+
+
+
+//5. slajdowanie zdjęć po kliknięciu kolejnych kropek
+/*    function slideChange(target) {
         carouselList.stop().animate({'left': -600 * target });
         dot.removeClass('active').eq(target).addClass('active');
         resetInterval();
-    };
+    };*/
     
-    //slajdowanie automatyczne
+//function moveAndActive(dotArrayIteration){};
+    
+    
+
+    
+    //6. slajdowanie automatyczne
+/*
     function slider(){     
         target = dot.siblings('.active').index();
         target == dot.length - 1 ? target = 0 : target += 1;
         slideChange(target);
-        
     };
-    //ustawienie interwału
-    interval = setInterval(function(){slider()}, 2000);
+*/
+        
+    //7. ustawienie interwału
+  //  interval = setInterval(function(){slider()}, 2000);
+//    https://stackoverflow.com/questions/15521081/jquery-animate-in-pure-javascript
     
-    
-    //prawy i lewy element do klikania
-    $('.right').click(function(){
+    //8. prawy i lewy element do klikania
+    /*$('.right').click(function(){
         target = target = dot.siblings('.active').index();
         target == dot.length - 1 ? target = 0 : target += 1;
         slideChange(target);
@@ -68,16 +103,15 @@ $(function() {
         target == 0 ? target = dot.length - 1 : target -= 1;
         slideChange(target);
         
-    });
+    });*/
     
-    //funkcja kasująca slajdowanie automamatyczne - wstawiona w momencie kliknięcia kolejnych kropek - patrz linia 28
-    function resetInterval(){
+    //9. funkcja kasująca slajdowanie automamatyczne - wstawiona w momencie kliknięcia kolejnych kropek - patrz linia 28
+    /*function resetInterval(){
         clearInterval(interval);
         interval = setInterval(function(){slider();}, 2000);
     };
     
 });
-
 */
 
 
